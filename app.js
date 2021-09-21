ccfdf376071684efce2078510a4e5ad3d4aac066
John Chavez <jrc2855@gmail.com>
2021-09-21 12:03:19 -0400

    lab 6 in progress

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 // Create separate objects for each shop and have it output the following to the sales.html file.
 'use strict'
 
+
+//let x = 1
+//let y = 10
+
+//function testFirst(){
+  //return Math.random() * (y - x) + x;
+
+
+
 let hoursOpen = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm']
+
+let list = "<ul>";
+hoursOpen.forEach(cookiesPerHour);
+list += "</ul>";
+
+
+function cookiesPerHour(avgSales){
+  list += "<li>" + avgSales + "</li>";
+  return Math.floor(Math.random() * (avgSales * customersPerHour()));
+  
+}
+
+function customersPerHour(min, max){
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
 
 let seattle = {
   "name": "Seattle",
   "min": 23,
   "max": 66,
   "avgSales": 6.3,
-  customers: function(){
-    seattlecust = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-    console.log(seattlecust);
-  }
 };
-
 
 let Tokyo = {
   "name": "Tokyo",
@@ -41,4 +62,4 @@ let Lima = {
   "min": 2,
   "max": 16,
   "avgSales": 4.6,
-};
+}
