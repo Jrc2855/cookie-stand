1e1cefd3a4816ea60f9570fd3d23bd03f7a58f31
John Chavez <jrc2855@gmail.com>
2021-09-21 20:52:18 -0400

    Tables for all stores

--- app.js
+++ app.js
@@ -8,10 +8,22 @@ function customersPerHour(min, max){
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+function generateLineItems(store) { //organizes everything into a line item for table display
+  let parentEl = document.querySelector(`#${store.name}`); //establishes the parent element as the stores name
+  for (let total = 0; total < store.totalSales.length; total++) { //for loop runs variable 'total' for as long as the number of stores.
+    let newItem = document.createElement('li'); // variable 'newItem' creates a new list item element
+    newItem.innerText = store.totalSales[total];
+    parentEl.appendChild(newItem); // appends (adds to the bottom) the 'newItem' element as a child element.
+  }
+  let lastItem = document.createElement('li');
+  lastItem.innerText = store.grandTotal;
+  parentEl.appendChild(lastItem);// appends last item to the bottom of the parent element (which is store)
+};
 
 
-let seattle = {
-  name: "seattle",
+
+let Seattle = {
+  name: "Seattle",
   min: 23,
   max: 66,
   avgSales: 6.3,
@@ -23,51 +35,103 @@ let seattle = {
 // ^ line above generates a random number between the minimum and maximum ranges
       let sales = Math.round(custGenerated * this.avgSales);
       this.totalSales.push(`${hoursOpen[i]}: Current Sales ${sales}`);
-      this.grandTotal = this.grandTotal + sales
+      this.grandTotal = this.grandTotal + sales 
+    }
+  },
+};
+console.log(Seattle);
+Seattle.hourlyCustomers();
+generateLineItems(Seattle);
+
+
+let Tokyo = {
+  name: "Tokyo",
+  min: 3,
+  max: 24,
+  avgSales: 1.2,
+  totalSales: [],
+  grandTotal: 0,
+  hourlyCustomers: function() {
+    for (let i = 0; i < hoursOpen.length; i++){
+      let custGenerated = customersPerHour(this.min, this.max);
+// ^ line above generates a random number between the minimum and maximum ranges
+      let sales = Math.round(custGenerated * this.avgSales);
+      this.totalSales.push(`${hoursOpen[i]}: Current Sales ${sales}`);
+      this.grandTotal = this.grandTotal + sales 
     }
   },
 };
 
-function generateLineItems(store) { //organizes everything into a line item for table display
-  let parentEl = document.querySelector(`#${store.name}`); //establishes the parent element as the stores name
-  for (let total = 0; total < store.totalSales.length; total++) { //for loop runs variable 'total' for as long as the number of stores.
-    let newItem = document.createElement('li'); // variable 'newItem' creates a new list item element
-    newItem.innerText = store.totalSales[total];
-    parentEl.appendChild(newItem); // appends (adds to the bottom) the 'newItem' element as a child element.
-  }
-  let lastItem = document.createElement('li');
-  lastItem.innerText = store.grandTotal;
-  parentEl.appendChild(lastItem);// appends last item to the bottom of the parent element (which is store)
-}
+console.log(Tokyo);
+Tokyo.hourlyCustomers();
+generateLineItems(Tokyo);
 
-console.log(seattle);
-seattle.hourlyCustomers();
-generateLineItems(seattle);
 
-let Tokyo = {
-  "name": "Tokyo",
-  "min": 3,
-  "max": 24,
-  "avgSales": 1.2,
-};
 
 let Dubai = {
-  "name": "Dubai",
-  "min": 11,
-  "max": 38,
-  "avgSales": 3.7,
+  name: "Dubai",
+  min: 11,
+  max: 38,
+  avgSales: 3.7,
+  totalSales: [],
+  grandTotal: 0,
+  hourlyCustomers: function() {
+    for (let i = 0; i < hoursOpen.length; i++){
+      let custGenerated = customersPerHour(this.min, this.max);
+// ^ line above generates a random number between the minimum and maximum ranges
+      let sales = Math.round(custGenerated * this.avgSales);
+      this.totalSales.push(`${hoursOpen[i]}: Current Sales ${sales}`);
+      this.grandTotal = this.grandTotal + sales 
+    }
+  },
 };
+
+console.log(Dubai);
+Dubai.hourlyCustomers();
+generateLineItems(Dubai);
+
 
 let Paris = {
-  "name": "Paris",
-  "min": 20,
-  "max": 38,
-  "avgSales": 2.3,
+  name: "Paris",
+  min: 20,
+  max: 38,
+  avgSales: 2.3,
+  totalSales: [],
+  grandTotal: 0,
+  hourlyCustomers: function() {
+    for (let i = 0; i < hoursOpen.length; i++){
+      let custGenerated = customersPerHour(this.min, this.max);
+// ^ line above generates a random number between the minimum and maximum ranges
+      let sales = Math.round(custGenerated * this.avgSales);
+      this.totalSales.push(`${hoursOpen[i]}: Current Sales ${sales}`);
+      this.grandTotal = this.grandTotal + sales 
+    }
+  },
 };
 
+console.log(Paris);
+Paris.hourlyCustomers();
+generateLineItems(Paris);
+
 let Lima = {
-  "name": "Lima",
-  "min": 2,
-  "max": 16,
-  "avgSales": 4.6,
-}
+  name: "Lima",
+  min: 2,
+  max: 16,
+  avgSales: 4.6,
+  totalSales: [],
+  grandTotal: 0,
+  hourlyCustomers: function() {
+    for (let i = 0; i < hoursOpen.length; i++){
+      let custGenerated = customersPerHour(this.min, this.max);
+// ^ line above generates a random number between the minimum and maximum ranges
+      let sales = Math.round(custGenerated * this.avgSales);
+      this.totalSales.push(`${hoursOpen[i]}: Current Sales ${sales}`);
+      this.grandTotal = this.grandTotal + sales 
+    }
+  },
+};
+
+console.log(Lima);
+Lima.hourlyCustomers();
+generateLineItems(Lima);
+
